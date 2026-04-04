a05ba9811ad5fa8ec50d295dad7f3d040add6d97
GlobalCodes <glorychukwuzurum@gmail.com>
2026-04-04 15:57:30 +0000

    feat: add Redis integration and update routes to use Redis client

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const helmet = require('helmet');
 const http = require('http')
 const connectDB = require('./src/config/db');
 const startSocket = require('./src/sockets/chatSocket');
+
 const authRoutes = require('./src/routes/authRoute')
 const serviceRoutes = require('./src/routes/serviceRoute');
 const projectRoutes = require('./src/routes/projectRoute');
-const cookieParser = require('cookie-parser');
+
 
 const app = express();
+
+const redisClient = Redis(process.env.REDIS_URL)
+
+redisClient.on('connect', ()=>{
+  console.log('Connected to Redis');
+})
+
+redisClient.on('error', (error)=>{
+  console.error('Error connecting to Redis', error);
+});
 
 connectDB();
 
@@ -23,9 +34,18 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/services', serviceRoutes);  
-app.use('/api/projects', projectRoutes);
+app.use('/api/auth', (req, res, next)=>{
+  req.redisClient = redisClient;
+  next();
+}, authRoutes);
+app.use('/api/services', (req, res, next)=>{
+  req.redisClient = redisClient;
+  next();
+}, serviceRoutes);
+app.use('/api/projects', (req, res, next)=>{
+  req.redisClient = redisClient;
+  next();
+}, projectRoutes);
 
 // Create Server
 const server = http.createServer(app);
